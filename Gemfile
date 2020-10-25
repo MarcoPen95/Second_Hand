7e0f4d9c2a6bef4b790af8aac5ebea88a5c6ed40
Dorjan Hysa <dorjan.hysa@gmail.com>
2020-10-25 21:30:53 +0100

    added spec file

--- Gemfile
+++ Gemfile
@@ -41,7 +41,8 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'better_errors', '~> 2.4'
   gem 'guard', '~> 2.14', '>= 2.14.1'
-  gem 'guard-livereload', '~> 2.5', '>= 2.5.2' 
+  gem 'guard-livereload', '~> 2.5', '>= 2.5.2'
+  gem 'rspec-rails', ">= 3.9.0"
 end
 
 
@@ -70,6 +71,7 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  
 end
 
 group :test do
---
+++ spec/models/buyer_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe Buyer, type: :model do
+  
+  describe 'Buyer navigation' do
+    describe 'Creation' do
+      before do
+        @buyer = Buyer.create(username: "pg", email: "test@test.com", password: "password", password_confirmation: "password")
+      end
+      it 'should be able to be created if valid' do
+        expect(@buyer).to be_valid
+      end
+
+      describe 'Validations' do
+        it 'should not be valid without a username' do
+          @buyer.username = nil
+          expect(@buyer).to_not be_valid
+        end
+        it 'should not be valid without an email' do
+          @buyer.email = nil
+          expect(@buyer).to_not be_valid
+        end
+      end
+
+      describe "Associations" do
+        it "has many favorites" do
+          assc = described_class.reflect_on_association(:favorites)
+          expect(assc.macro).to eq :has_many
+        end
+      end
+    end
+  end  
+end
